2dc6f970a69b7b05c8bedd8f9401b5daf4e16711
Phoeni10 <webjscreator@gmail.com>
2023-12-22 11:33:19 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr = '';
+  for (let i = 1; i <= options.repeatTimes; i += 1) {
+    newStr += str;
+    for (let j = 1; j <= options.additionRepeatTimes; j += 1) {
+      newStr += options.addition;
+      if (j < options.additionRepeatTimes) {
+        newStr += options.additionSeparator;
+      }
+    }
+    if (i < options.repeatTimes) {
+      newStr += options.separator;
+    }
+  }
+  return newStr;
 }
 
 module.exports = {
